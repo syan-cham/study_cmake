e178a6f571aa2be02f958171462dc1d26f2ba9ad
eglinux <xchaojun_525@sina.com>
2023-02-16 17:49:53 +0800

    modify: a complate c/c++ cmake template.
    Signed-off-by: eglinux <xchaojun_525@sina.com>

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,25 @@
 #include <vector>
 #include <nlohmann/json.hpp>
 
+#include "add.h"
+
 using json_t = nlohmann::json;
 
 int main()
 {
-    std::cout << sizeof(uint64_t) << std::endl;
+    int a = 1;
+    int b = 2;
+
+    std::cout << study::add(a, b) << std::endl;
+
+    auto j = R"(
+    {
+        "happy": true,
+        "pi": 3.141
+    }
+    )"_json;
+
+    std::string s = j.dump();
+
+    std::cout << s << std::endl;
 }
